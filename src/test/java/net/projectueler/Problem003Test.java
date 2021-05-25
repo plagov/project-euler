9e0f50582fc6df485d01db25d005d763d1a9779a
Vitalii Plagov <vitalii.plagov@gmail.com>
2021-05-26 00:23:16 +0300

    Solve problem 003

---
+++ src/test/java/net/projectueler/Problem003Test.java
@@ -0,0 +1,14 @@
+package net.projectueler;
+
+import java.util.Collections;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Problem003Test {
+
+  @Test
+  void highestPrimeFactorOfTheNumber() {
+    var primeFactors = new Problem003().getPrimeFactorsOfTheNumber(600_851_475_143L);
+    Assertions.assertEquals(Collections.max(primeFactors), 6857);
+  }
+}
