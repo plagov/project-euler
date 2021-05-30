9342aab8fc9cc32215fd359a87669e92e106397a
Vitalii Plagov <vitalii.plagov@gmail.com>
2021-05-31 01:13:51 +0300

    Minimum working solution

--- src/main/java/net/projecteuler/Problem004.java
+++ src/main/java/net/projecteuler/Problem004.java
@@ -1,11 +1,34 @@
 package net.projecteuler;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
 public class Problem004 {
 
   private boolean isPalindrome(Long number) {
     var original = number.toString();
     var reversed = new StringBuilder(original).reverse().toString();
     return original.equals(reversed);
+  }
+
+  public Set<Long> palindromeNumbers() {
+
+    Set<Long> palindromes = new HashSet<>();
+
+    LongStream.range(100, 1000).forEach(first -> {
+      LongStream.range(100, 1000).forEach(second -> {
+        var product = first * second;
+        if (isPalindrome(product)) {
+          palindromes.add(product);
+        }
+      });
+    } );
+
+    return palindromes;
   }
 
 }
