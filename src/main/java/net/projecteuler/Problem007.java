70bed4eb680a96f88597cbf03804e75c4ef0fa41
Vitali Plagov <vitalii.plagov@gmail.com>
2021-07-29 12:19:44 +0300

    Solve problem 007 (#8)
    * Method to check whether a number is a prime number
    
    * Method to get prime number by index
    
    This algorithm is not ideal. Actually, it is quite insufficient since it
    does extra operations. There must be a more suitable algorithm, but I
    don't know know how to implement it. Thus, I made this one which does
    the job in a brute-force mode.
    
    Also, add two tests for sample input and for task input.
    
    * Remove unnecessary comment
    
    * Use Java 11

---
+++ src/main/java/net/projecteuler/Problem007.java
@@ -0,0 +1,31 @@
+package net.projecteuler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem007 {
+
+  public int primeNumberByIndex(int index) {
+    List<Integer> primes = new ArrayList<>();
+    int number = 2;
+    while (primes.size() < index) {
+      if (isPrimeNumber(number)) {
+        primes.add(number);
+      }
+      number++;
+    }
+    return primes.get(primes.size() - 1);
+  }
+
+  private boolean isPrimeNumber(int number) {
+    int divisibleNumber = 2;
+    while (number != divisibleNumber) {
+      if (number % divisibleNumber == 0) {
+        return false;
+      }
+      divisibleNumber++;
+    }
+    return true;
+  }
+
+}
