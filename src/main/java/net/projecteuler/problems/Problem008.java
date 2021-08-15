db531a69249635df316e830be2a5dbb3923ad46e
Vitalii Plagov <vitalii.plagov@gmail.com>
2021-08-15 15:27:48 +0300

    Solve Problem 8
    Find the highest product of adjacent digits.

--- src/main/java/net/projecteuler/problems/Problem008.java
+++ src/main/java/net/projecteuler/problems/Problem008.java
@@ -1,15 +1,35 @@
 package net.projecteuler.problems;
 
+import net.projecteuler.util.FileUtil;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import net.projecteuler.util.FileUtil;
 
 public class Problem008 {
 
   private List<Integer> getListOfDigits() {
     String string = FileUtil.readInputFile("problem008.txt").replace("\n", "");
     return Arrays.stream(string.split("")).map(Integer::valueOf).collect(Collectors.toList());
+  }
+
+  public long highestProductOfAdjacentNumberOfDigit(int numberOfAdjacentDigits) {
+    var allDigits = getListOfDigits();
+    var startIndex = 0;
+    var endIndex = numberOfAdjacentDigits;
+    List<Long> products = new ArrayList<>();
+    while (endIndex != allDigits.size()) {
+      var adjacentDigits = allDigits.subList(startIndex, endIndex);
+      if (!adjacentDigits.contains(0)) {
+        var product = adjacentDigits.stream().mapToLong(a -> a).reduce(1, (a, b) -> a * b);
+        products.add(product);
+      }
+      startIndex++;
+      endIndex++;
+    }
+    return Collections.max(products);
   }
 
 }
