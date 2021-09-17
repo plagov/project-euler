3b84d4553d2fea5eb2eb66777d0f4ca19f454eb0
Vitalii Plagov <vitalii.plagov@gmail.com>
2021-09-17 23:24:29 +0300

    Extract variables to instance fields

--- src/main/java/net/projecteuler/problems/Problem009.java
+++ src/main/java/net/projecteuler/problems/Problem009.java
@@ -2,11 +2,13 @@ package net.projecteuler.problems;
 
 public class Problem009 {
 
+  private long x, y, z;
+  private long sum;
+  private long v, u;
+
   public long productOfPythagoreanTripletWhichSumIsEqualTo(int sumOfPythagoreanTriplet) {
-    long v = 1;
-    long u = 2;
-    long x = 0, y = 0, z = 0;
-    long sum = 0;
+    v = 1;
+    u = 2;
 
     while (sum != sumOfPythagoreanTriplet) {
       x = (long) (Math.pow(u, 2) - Math.pow(v, 2));
