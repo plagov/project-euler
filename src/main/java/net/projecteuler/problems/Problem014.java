5a01f296fd2bc1008fec5e4305fdfce809d0e094
Vitali Plagov <vplagov@pm.me>
2022-01-15 13:56:58 +0200

    Calculate Collatz Sequence for a number

--- src/main/java/net/projecteuler/problems/Problem014.java
+++ src/main/java/net/projecteuler/problems/Problem014.java
@@ -1,5 +1,7 @@
 package net.projecteuler.problems;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -8,9 +10,9 @@ import java.util.stream.IntStream;
 public class Problem014 {
 
   public int solve() {
-    return IntStream.rangeClosed(1, 1_000_000)
+    return IntStream.rangeClosed(1, 100_000)
       .boxed()
-      .collect(Collectors.toMap(Function.identity(), this::sizeOfCollatzSequenceForNumber))
+      .collect(Collectors.toMap(Function.identity(), n -> collatzSequenceForNumber(n).size()))
       .entrySet()
       .stream()
       .max(Map.Entry.comparingByValue())
@@ -18,16 +20,18 @@ public class Problem014 {
       .getKey();
   }
 
-  private int sizeOfCollatzSequenceForNumber(int number) {
-    var size = 1;
+  public List<Integer> collatzSequenceForNumber(int number) {
+    List<Integer> sequence = new ArrayList<>();
+    sequence.add(number);
     while (number != 1) {
       if (number % 2 == 0) {
         number = number / 2;
+        sequence.add(number);
       } else {
         number = 3 * number + 1;
+        sequence.add(number);
       }
-      size++;
     }
-    return size;
+    return sequence;
   }
 }
--- src/test/java/net/projecteuler/problems/Problem014Test.java
+++ src/test/java/net/projecteuler/problems/Problem014Test.java
@@ -1,15 +1,31 @@
 package net.projecteuler.problems;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class Problem014Test {
 
   private final Problem014 problem14 = new Problem014();
 
   @Test
+  @Disabled
   void solve() {
     var numberOfTerms = problem14.solve();
     Assertions.assertEquals(42, numberOfTerms);
+  }
+
+  @Test
+  void testCollatzSequence() {
+    Assertions.assertEquals(
+      List.of(12, 6, 3, 10, 5, 16, 8, 4, 2, 1),
+      problem14.collatzSequenceForNumber(12)
+    );
+    Assertions.assertEquals(
+      List.of(25, 76, 38, 19, 58, 29, 88, 44, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1),
+      problem14.collatzSequenceForNumber(25)
+    );
   }
 }
