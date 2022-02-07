0fb3576bb040ca5411e18c1cfd6ea7e605cdd09a
Vitali Plagov <vplagov@pm.me>
2022-02-07 22:59:24 +0200

    Extract max path sum algorithm & solve problem 67
    The same algorithm is used for both problem 18 and problem 67. Extract
    it into a separate method and use for different input

--- src/main/java/net/projecteuler/problems/Problem067.java
+++ src/main/java/net/projecteuler/problems/Problem067.java
@@ -5,9 +5,9 @@ import net.projecteuler.util.MaximumPathSum;
 
 import java.util.Arrays;
 
-public class Problem018 {
+public class Problem067 {
   public Long solve() {
-    var input = Arrays.stream(FileUtil.readInputFile("problem018.txt").split("\n"))
+    var input = Arrays.stream(FileUtil.readInputFile("problem067.txt").split("\n"))
       .map(line -> Arrays.stream(line.split(" ")).map(Long::valueOf).toList()).toList();
 
     return MaximumPathSum.maximumPathSum(input);
---
+++ src/main/java/net/projecteuler/util/MaximumPathSum.java
@@ -0,0 +1,33 @@
+package net.projecteuler.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class MaximumPathSum {
+
+  public static Long maximumPathSum(List<List<Long>> listOfRows) {
+    var finalList = new ArrayList<>(listOfRows);
+
+    for (var i = listOfRows.size() - 1; i-- > 0; ) {
+      List<Long> listOfSums = new ArrayList<>();
+      for (var k = 0; k < listOfRows.get(i).size(); k++) {
+        var currentLine = listOfRows.get(i);
+        var belowLine = finalList.get(finalList.size() - 1);
+        var firstSum = currentLine.get(k) + belowLine.get(k);
+        var secondSum = currentLine.get(k) + belowLine.get(k + 1);
+        listOfSums.add(Math.max(firstSum, secondSum));
+      }
+      finalList.remove(finalList.size() - 2);
+      finalList.remove(finalList.size() - 1);
+      finalList.add(listOfSums);
+    }
+
+    var flattenList = finalList.stream().flatMap(Collection::stream).toList();
+    if (flattenList.size() != 1) {
+      throw new IllegalArgumentException("Final list should contain only one element, but has: " + flattenList);
+    }
+
+    return flattenList.get(0);
+  }
+}
