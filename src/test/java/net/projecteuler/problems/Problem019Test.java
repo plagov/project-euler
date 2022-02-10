9bdbe70f736916f91472d23810c4c0b766d1e28e
Vitali Plagov <vplagov@pm.me>
2022-02-10 07:20:08 +0200

    Solve Problem 19
    Used Java Date API to solve the problem with an easy and clear solution.

---
+++ src/test/java/net/projecteuler/problems/Problem019Test.java
@@ -0,0 +1,13 @@
+package net.projecteuler.problems;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Problem019Test {
+
+  @Test
+  void solve() {
+    assertEquals(171, new Problem019().solve());
+  }
+}
